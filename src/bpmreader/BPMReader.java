71dc6c60d60c0efd0cdfc4ee7427b0d806292c6a
Adrián Portillo Sánchez <adri95cadiz@correo.ugr.es>
2017-02-20 14:51:57 +0100

    Mejorado sistema en general, terminado para pasar a agente.

--- src/bpmreader/BPMReader.java
+++ src/bpmreader/BPMReader.java
@@ -26,8 +26,12 @@ public class BPMReader {
 
     private final static int MIN_TEMPO_BPM = 60;
     private final static int MAX_TEMPO_BPM = 220;
-    private final static int DEFAULT_TEMPO = 112;
     private final static int MAX_NOTES_KEY = 15;
+    //Para el tempo total.
+    private static double StartTime_ms = 0;
+    private static double FinishTime_ms = 0;
+    private static double CurrentTime_ms = 0;
+    private static int NumberSamples = 0;
     /**
      * @param args the command line arguments
      * @throws javax.sound.sampled.LineUnavailableException
@@ -42,17 +46,15 @@ public class BPMReader {
             //Ultimos valores.
             private double lastProbability = 0;          
             private double lastTimeStamp = 0;         
-            private double lastPitch = 0;                    
-            private double lastBpm = DEFAULT_TEMPO;              
-            private double adaptedBpm = 0;
-            //Para el tempo total.
-            private double StartTime_ms = 0;
-            private double FinishTime_ms = 0;
-            private double CurrentTime_ms = 0;
-            private int NumberSamples = 0;
+            private double lastPitch = 0;               
+            private double bpm = 0;
+            private double bpmBeat = 0;
+            private int beat = 4;
+            private int octava;
+            private int nota;
             //Key Finder
             private KeyFinder keyFinder = new KeyFinder("Unknown", "Unknown");
-            private ArrayList<String> noteArray = new ArrayList<String>();
+            private ArrayList<String> noteArray = new ArrayList<>();
             @Override
             public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
                 if(pitchDetectionResult.getPitch() != -1){
@@ -60,8 +62,6 @@ public class BPMReader {
                     float pitch = pitchDetectionResult.getPitch();
                     float probability = pitchDetectionResult.getProbability();
                     double rms = audioEvent.getRMS() * 100;
-                    //String message = String.format("\nFrecuencia detectada: %.2fs: %.2fHz ( %.2f probabilidad, media cuadratica: %.5f )", timeStamp,pitch,probability,rms);
-                    //System.out.println(message);   
                     double difPitch = (pitch - lastPitch);
                     double timeBetween = (timeStamp - lastTimeStamp);
                     if(timeStamp > 0.5 && (difPitch > 5 || difPitch < -5 || timeBetween > 0.1)){ 
@@ -69,88 +69,71 @@ public class BPMReader {
                             if(difPitch > 0.5 || difPitch < -0.5){
                                 if (difPitch > 2 || difPitch < -2 || probability > lastProbability + 0.1 || (lastProbability > 0.95 && probability > lastProbability + 0.005)) {
                                     
-                                    System.out.println("\nBeat detectado en: " + timeStamp + " con frecuencia de: " + pitch);                                
-                                    System.out.println("Y media cuadratica de: " + rms);
-                                    //System.out.println(probability + " " + lastProbability);
-                                    //System.out.println(difPitch + " = " + pitch + " " + lastPitch);
-                                    System.out.println("Tiempo desde el anterior beat: " + timeBetween);
+                                    System.out.println("\nBeat detectado en: " + timeStamp + " s con frecuencia de: " + pitch +" Hz");                                
+                                    System.out.println("Media cuadratica de: " + rms + " y probabilidad de: " + probability);
+                                    System.out.println("Tiempo desde el anterior beat: " + timeBetween + " s");
                                     lastTimeStamp = timeStamp;
                                     lastPitch = pitch;
                                     CurrentTime_ms = timeStamp;
                                     if (NumberSamples == 0) {
-                                            StartTime_ms = CurrentTime_ms;
+                                        StartTime_ms = CurrentTime_ms;
                                     }
                                     NumberSamples++;
-                                    FinishTime_ms = CurrentTime_ms;                                    
-                                    double bpmFull = 0;
+                                    FinishTime_ms = CurrentTime_ms;   
                                     if (NumberSamples >= 2 && (FinishTime_ms - StartTime_ms) != 0) { 
-                                        bpmFull = (((NumberSamples -1) * 1000 * 60) / FinishTime_ms - StartTime_ms) / 1000;
+                                        bpm = (((NumberSamples -1) * 1000 * 60) / FinishTime_ms - StartTime_ms) / 1000;
                                     }                                    
-                                    System.out.println("bpm totales: " + bpmFull);
-                                    int beat = 4;
-                                    double bpm = 0;
+                                    System.out.println("BPM Totales: " + bpm + " bpm");
+                                    beat = 4;
                                     if(timeBetween != timeStamp){
-                                        bpm = 60/timeBetween;
-                                        System.out.println("bpm crudo = " + bpm);
-                                        while(bpm < MIN_TEMPO_BPM){
-                                            bpm*=2;  
+                                        bpmBeat = 60/timeBetween;
+                                        System.out.println("BPM Crudo: " + bpmBeat + " bpm");
+                                        while(bpmBeat < MIN_TEMPO_BPM || (Math.abs((bpmBeat*2) - bpm)) < (Math.abs((bpmBeat) - bpm)) ){
+                                            bpmBeat*=2;  
                                             beat/=2;
                                         }
-                                        while(bpm > MAX_TEMPO_BPM){
-                                            bpm/=2;
+                                        while(bpmBeat > MAX_TEMPO_BPM || (Math.abs((bpmBeat/2) - bpm)) < (Math.abs((bpmBeat) - bpm)) ){
+                                            bpmBeat/=2;
                                             beat*=2;
                                         }
                                     }
-                                    if(adaptedBpm == 0){
-                                        adaptedBpm = bpm;
-                                    }
-                                    System.out.println("bpm neto = " + bpm);
-                                    if(bpm > lastBpm){                 
-                                        adaptedBpm++;
-                                        System.out.println("Bpm en aumento. Bpm adaptados = " + adaptedBpm);  
-                                    } else{
-                                        adaptedBpm--;
-                                        System.out.println("Bpm en disminución. Bpm adaptados = " + adaptedBpm);
-                                    }
-                                    System.out.println("Duracion del beat = 1/" + beat);
-                                    lastBpm = bpm;     
-                                    int note = (int) Math.floor(Math.log(pitch/440.0)/Math.log(2.0) * 12 + 69);
-                                    System.out.println("Nota = " + note);
-                                    int octava;
-                                    int nota;
-                                    if(note < 12) {
+                                    System.out.println("BPM Neto: " + bpmBeat + " bpm");
+                                    System.out.println("Duracion del Beat: 1/" + beat);
+                                    int noteMIDI = (int) Math.floor(Math.log(pitch/440.0)/Math.log(2.0) * 12 + 69);
+                                    System.out.println("Nota MIDI: " + noteMIDI);
+                                    if(noteMIDI < 12) {
                                         octava = 0;
-                                        nota = note;
-                                    } else if(note < 24){                                    
+                                        nota = noteMIDI;
+                                    } else if(noteMIDI < 24){                                    
                                         octava = 1;
-                                        nota = note - 12;
-                                    } else if(note < 36){
+                                        nota = noteMIDI - 12;
+                                    } else if(noteMIDI < 36){
                                         octava = 2;
-                                        nota = note - 24;
-                                    } else if(note < 48){
+                                        nota = noteMIDI - 24;
+                                    } else if(noteMIDI < 48){
                                         octava = 3;
-                                        nota = note - 36;
-                                    } else if(note < 60){
+                                        nota = noteMIDI - 36;
+                                    } else if(noteMIDI < 60){
                                         octava = 4;
-                                        nota = note - 48;
-                                    } else if(note < 72){  
+                                        nota = noteMIDI - 48;
+                                    } else if(noteMIDI < 72){  
                                         octava = 5;  
-                                        nota = note - 60;                                    
-                                    } else if(note < 84){
+                                        nota = noteMIDI - 60;                                    
+                                    } else if(noteMIDI < 84){
                                         octava = 6;
-                                        nota = note - 72;
-                                    } else if(note < 96){
+                                        nota = noteMIDI - 72;
+                                    } else if(noteMIDI < 96){
                                         octava = 7;
-                                        nota = note - 80;
-                                    } else if(note < 108){
+                                        nota = noteMIDI - 80;
+                                    } else if(noteMIDI < 108){
                                         octava = 8;
-                                        nota = note - 96;
-                                    } else if(note < 120){
+                                        nota = noteMIDI - 96;
+                                    } else if(noteMIDI < 120){
                                         octava = 9;
-                                        nota = note - 108;
+                                        nota = noteMIDI - 108;
                                     } else {
                                         octava = 10;
-                                        nota = note - 120;
+                                        nota = noteMIDI - 120;
                                     }
                                     switch(nota){
                                         case 0: 
@@ -216,10 +199,15 @@ public class BPMReader {
                     lastProbability = probability;
                 }
             }              
-        };
+        };      
         
         AudioDispatcher adp = AudioDispatcherFactory.fromDefaultMicrophone(sampleRate, bufferSize, 0);
         adp.addAudioProcessor(new PitchProcessor(PitchEstimationAlgorithm.YIN, sampleRate, bufferSize, pitchhandler));
         adp.run();
+    }
+    
+    public static void resetBPM(){
+        NumberSamples = 0;
+        StartTime_ms = CurrentTime_ms;
     }
 }
